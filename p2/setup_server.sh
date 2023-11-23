4414409c75beea07c6a503a6e2db02a81925c387
Jaemoo Jung <jaemjung@made-f0Dr4s1.clusters.42paris.fr>
2023-11-23 14:33:58 +0100

    [feat] create machine for p2

---
+++ p2/setup_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install k3s
+curl -sfL https://get.k3s.io | sh -s - \
+    --node-ip 192.168.56.110 \
+    --advertise-address 192.168.56.110 \
+    --flannel-iface eth1 \
+    --write-kubeconfig-mode 644
+
+# Set alias for kubectl
+echo "alias k='kubectl'" >> /home/vagrant/.bashrc

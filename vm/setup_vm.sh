74134764dd33ed81a72d9a39a4e7b25c03c550f7
Jaemoo Jung <jaemjung@made-f0Dr6s7.clusters.42paris.fr>
2023-12-19 16:23:47 +0100

    [fix] change base vm name

--- vm/setup_vm.sh
+++ vm/setup_vm.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#install ubuntu desktop
-sudo apt-get install -y --no-install-recommends ubuntu-desktop
-
 #install vscode
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install gnupg2 -y
@@ -11,9 +8,10 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update -y
 sudo apt-get install -y code
-
-#install git
 sudo apt-get install -y git
+
+#install ubuntu desktop
+sudo apt-get install -y --no-install-recommends ubuntu-desktop
 
 #install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb

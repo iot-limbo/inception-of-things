a94574052e5867197b8fdd1c1a7776b0df9b35d4
Jaemoo Jung <jaemjung@made-f0Dr9s14.clusters.42paris.fr>
2023-12-21 15:49:28 +0100

    [fix] change p3 conf path on argocd setup script

--- p3/scripts/setup_argocd.sh
+++ p3/scripts/setup_argocd.sh
@@ -8,20 +8,20 @@ sudo kubectl create namespace dev
 sudo kubectl create namespace argocd
 sleep 3
 
-sudo kubectl apply -f /vagrant/confs/argocd_install.yaml -n argocd
+sudo kubectl apply -f ../confs/argocd_install.yaml -n argocd
 sleep 3
 
 sudo kubectl wait --for=condition=Ready pods --all -n argocd
-sleep 3
 
 # Apply ingress
-sudo kubectl apply -f /vagrant/confs/argocd_ingress.yaml -n argocd
+sudo kubectl apply -f ../confs/argocd_ingress.yaml -n argocd
 sleep 3
 
 # Apply project and application
-sudo kubectl apply -f /vagrant/confs/project.yaml -n argocd
+sudo kubectl apply -f ../confs/project.yaml -n argocd
 sleep 3
-sudo kubectl apply -f /vagrant/confs/application.yaml -n argocd
+
+sudo kubectl apply -f ../confs/application.yaml -n argocd
 sleep 3
 
 echo "ArgoCD is ready at http://localhost:8080"
